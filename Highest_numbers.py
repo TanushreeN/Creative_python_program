644c0928d37193c35f5545c72cb8746380173d97
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-05-26 16:04:18 +0530

    Create Highest_numbers.py

---
+++ Highest_numbers.py
@@ -0,0 +1,10 @@
+#program to get only the required number of highest numbers from list where noumber of highest requirements is user input
+
+a = int(input("Enter a number from 1 to 6: "))
+numbers = [81, 52, 45, 3, 2, 96] 
+sorted_list = sorted(numbers)
+print(sorted_list)
+
+for num in numbers[(a-2)::-1]:
+ print(num, end =" ")
+print(numbers[-1])
