aede16b91d80e5045001cb3fc8b09da389741897
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-06-02 16:04:29 +0530

    Create remove_char_and capt.py

---
+++ remove_char.py
@@ -0,0 +1,11 @@
+# Input : geeks_for_geeks              Output : GeeksforGeeks
+# Input : left_index                   Output : leftIndex
+___________________________________
+
+def remove(string, char_remove):
+    return string.replace(char_remove, "")
+
+string = "geeks_for_geeks".title()
+char_remove = "_"
+result = remove(string, char_remove)
+print(result)
