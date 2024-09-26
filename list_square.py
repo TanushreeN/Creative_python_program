48cc8ab94f8e59dd4f6218ddd1b05d3ddaadeb63
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-09-26 07:15:22 +0530

    list_square.py

--- list_square.py
+++ list_square.py
@@ -6,3 +6,15 @@ print(squares)
 
 # OP : Enter the number till you wan't the square : 10
 #      [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+
+# ____________________________________________
+
+
+# Cubes using list
+n = int(input("Enter the number till you wan't the cubes : "))
+cubes = [x ** 3 for x in range(n+1)]
+print(cubes)
+
+# Enter the number till you wan't the cubes : 10
+# [0, 1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]
