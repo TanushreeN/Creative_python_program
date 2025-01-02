665db6605dd8e69641b4f7df6be34fe5d295a6f2
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2025-01-02 09:57:51 +0530

    left.py

--- left.py
+++ left.py
@@ -1,5 +1,6 @@
 # print the List1 elements which are not present in List2
 
+
 List1=[ 'nrupatunga', 'tanu', 'nidhi', 'papu', 'muddu', 'pingu']
 List2= ['pingu', 'muddu', 'nidhi']
 List3 = list(set(List1) - set(List2))
